f83c204cb1d41c5558726a630765f0be4b558a75
obidikejekwu <obidike.jekwu@gmail.com>
2022-07-12 14:34:08 +0100

    initial commit

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,2 +1,18 @@
 public class AreaCalculator {
+
+    public static final double INVALID_AREA = -1.0;
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return INVALID_AREA;
+        }
+        return radius * radius * Math.PI;
+    }
+
+    public static double area(double x, Double y) {
+        if (x < 0 || y < 0) {
+            return INVALID_AREA;
+        }
+        return x * y;
+    }
 }
